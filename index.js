139432e1cf977bac4385c6e77634c98d3b238f07
Stephen Enke <stephenenke@gmail.com>
2020-08-25 22:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function nowServing(katzDeliLine) {
 }
 function currentLine(katzDeliLine) {
   var line = [];
-  for (i = 0, i < katzDeliLine.length, )
+  for (i = 0, i < katzDeliLine.length, i++) {
+    
+  }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
