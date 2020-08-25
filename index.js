123976a4f4cc3c9731e84bf38d32c2b4f219841e
Stephen Enke <stephenenke@gmail.com>
2020-08-25 22:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
     for (i = 0; i < katzDeliLine.length; i++) {
     line += (i+1) + `. `  + katzDeliLine[i] + ", ";
   }
-  line.slice 
+  line = line.slice(0, -2)
   if (katzDeliLine.length === 0) {
     return `The line is currently: ${line}`
   }
