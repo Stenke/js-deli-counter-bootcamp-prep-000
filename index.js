9c461ccf1c1c8154864b7e80514862d6307adea6
Stephen Enke <stephenenke@gmail.com>
2020-08-25 21:34:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing(katzDeliLine) {
+  
+}
